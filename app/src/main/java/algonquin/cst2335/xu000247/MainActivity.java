0ee77558aa7c4f21c7c9d4c86d79bf6ea61a249c
xiaomeng831 <bradyxu831@gmail.com>
2021-06-24 14:04:58 -0400

    temporary storage

--- app/src/main/java/algonquin/cst2335/xu000247/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/xu000247/MainActivity.java
@@ -3,6 +3,10 @@ package algonquin.cst2335.xu000247;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.textView);
+        EditText et = findViewById(R.id.editText);
+        Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(clk -> {
+            String password = et.getText().toString();
+
+            checkPasswordComplexity( password );
+        });
+    }
+
+    /** This function is used to check whether the password contains an Upper Case letter,
+     * a lower case letter, a number, and a special symbol.
+     *
+     * @param pw The String object that we are checking
+     * @return Returns true if....
+     */
+    boolean checkPasswordComplexity( String pw ){
+        boolean foundUpperCase, foundLowerCase, foundNumber, foundSpecial;
+        foundUpperCase = foundLowerCase = foundNumber = foundSpecial = false;
+
+        if(!foundUpperCase) {
+            Toast.makeText("Need a Uppercase letter") ;// Say that they are missing an upper case letter;
+            return false;
+        }
+
+        else if( ! foundLowerCase) {
+            Toast.makeText( ... ); // Say that they are missing a lower case letter;
+            return false;
+        }
+        else if( ! foundNumber) { ....}
+        else if(! foundSpecial) { ... }
+        else
+            return true; //only get here if they're all true
     }
 }
